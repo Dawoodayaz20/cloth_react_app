38aeba83720316d035a7940bd391adbcdb207cd9
Dawood Ayaz <dawoodayaz18@gmail.com>
2025-03-16 07:30:16 +0500

    creating header component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import HomePage from './pages/homePage/homepage.component';
 import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/homePage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header  from './components/header/header.component';
 
 function App() {
   return( 
     <div>
+      <Header />
       <Routes> {/* Wrap Route elements inside <Routes> */}
         <Route path='/' element={<HomePage />} />
         <Route path='/shop' element={<ShopPage />} />
