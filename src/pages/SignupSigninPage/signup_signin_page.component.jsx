be4dfcee985577481a68875b43a5a2b03a34e44d
Dawood Ayaz <dawoodayaz18@gmail.com>
2025-03-25 16:14:54 +0500

    made sign-in page with sign-up form.

--- src/pages/SignupSigninPage/signup_signin_page.component.jsx
+++ src/pages/SignupSigninPage/signup_signin_page.component.jsx
@@ -3,10 +3,12 @@ import React from "react";
 import './signup_signin_page.styles.scss'
 
 import SignIn from '../../components/sign-in/signin.component'
+import SignUp from "../../components/sign-up/sign-up.component";
 
 const SignInSignUp = () => (
     <div className="'sign-in-and-sign-up'">
         <SignIn />
+        <SignUp />
     </div>
 )
 
